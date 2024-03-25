f1cda9830bb6b31c37c0409698cf89149472fc8f
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-03-26 05:22:26 +0530

    os modules

---
+++ os_module/os.js
@@ -0,0 +1,16 @@
+const os=require('os')
+// console.log("CPU ARCHITECTURE : "+ os.arch)
+
+// console.log("CPU FREE MEMORY :"+ os.freemem)
+
+// console.log("CPU TOTAL MEMORY :"+os.totalmem)
+
+// console.log("CPU NETWORK INTERFACE :"+os.networkInterfaces)
+
+// console.log("CPU temprory folder :"+os.tmpdir)
+
+console.log("endianness :"+os.endianness)
+console.log("os host name :"+os.hostname)
+console.log("os type :"+os.type)
+console.log("os platform :"+os.platform)
+console.log("os relese date :"+os.release)
