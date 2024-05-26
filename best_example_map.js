b4dbdb8d78bc2d0011a818a26040dee2c2d42444
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-05-26 21:15:28 +0530

    explanation of map

---
+++ best_example_map.js
@@ -0,0 +1,13 @@
+
+// --------------------------jab scope open kiye tho return keyword use karna paraga
+let num=[1,2,4,5,7]
+let newnum=num.map((p)=>{
+    return p
+});
+console.log(newnum)
+
+
+// -----------------------------jab scope open nhi kiya tho no returen
+let num1=[1,2,4,5,7]
+let newnum1=num1.map((np)=> np+10)
+console.log(newnum1)
